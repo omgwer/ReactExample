e5c321cd74ae207eccd1bd101424d697495817fe
aleksandr <hersgory2134@gmail.com>
2023-06-10 23:24:07 +0300

    feat: add react example components

--- src/hooks/products.ts
+++ src/hooks/products.ts
@@ -8,6 +8,10 @@ export  function useProducts() {
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState("")
 
+    function addProduct(product:IProduct) {
+        setProducts(prev => [...prev, product])
+    }
+
     async function fetchProducts() {
         try {
             setError('')
@@ -27,5 +31,5 @@ export  function useProducts() {
     }, [])
 
 
-    return {products, error, loading}
+    return {products, error, loading, addProduct}
 }
---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,7 @@
+
+
+export function AboutPage() {
+    return (
+        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, aperiam atque cum eius exercitationem in minima nostrum obcaecati perspiciatis quod velit voluptatibus. Adipisci aliquam architecto asperiores cumque deleniti, dignissimos dolor doloribus eius eos est eveniet fugit hic laborum maxime molestiae nihil nulla officia quae rem similique sunt unde velit veniam. </div>
+    )
+}
